bd2bfe41f04aa1cdabf906f8c0777ef31ef05b20
0x1771 <fakesmileux@gmail.com>
2024-10-11 22:08:41 +0300

    first commit

---
+++ static/js/hooks/use-texture-loader.js
@@ -0,0 +1,15 @@
+import {
+    useLoader
+} from "react-three-fiber";
+import * as THREE from "three";
+
+const useTextureLoader = (urlOrURLs) => {
+    const textureOrTextures = useLoader(
+        THREE.TextureLoader,
+        urlOrURLs,
+        (loader) => {}
+    );
+    return textureOrTextures;
+};
+
+export default useTextureLoader;
